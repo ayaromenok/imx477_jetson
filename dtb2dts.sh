2d2dbd40519e889f568892114b01e52287c109fd
Andrey Yaromenok <ayaromenok@gmail.com>
2021-05-01 04:55:10 +0300

    initial commit with IMX477 sensor

---
+++ dtb2dts.sh
@@ -0,0 +1,1 @@
+dtc -I dtb -O dts backup/tegra194-p3668-all-p3509-0000-imx477.dtb -o tegra194-p3668-all-p3509-0000-imx477.dts
---
+++ dts2dtb.sh
@@ -0,0 +1,1 @@
+dtc -I dts -O dtb tegra194-p3668-all-p3509-0000-imx477.dts -o out/tegra194-p3668-all-p3509-0000-imx477.dtb
